93e33a9ec2479f1df7620edbb6ccf803f98b2ca9
AdrianSA <Adrian.angara@gmail.com>
2020-05-02 13:56:12 -0400

    Implemented UI for edited and deleting Contestant

--- src/app/modules/owner/owner.module.ts
+++ src/app/modules/owner/owner.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OwnerDashboardComponent } from './components/owner-dashboard/owner-dashboard.component';
 import {RouterModule} from '@angular/router';
-import {MatDialogModule, MatFormFieldModule} from '@angular/material';
+import {MatDialogModule, MatFormFieldModule, MatIconModule, MatMenuModule} from '@angular/material';
 import { AddContestantComponent } from './components/add-contestant/add-contestant.component';
 import {ReactiveFormsModule} from '@angular/forms';
 
@@ -14,6 +14,8 @@ import {ReactiveFormsModule} from '@angular/forms';
   imports: [
     CommonModule,
     MatDialogModule,
+    MatIconModule,
+    MatMenuModule,
     ReactiveFormsModule,
     RouterModule.forChild([
       {
