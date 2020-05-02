4d9cd5f5ed2a5c0ad9b49324e446e1c8546229f6
AdrianSA <Adrian.angara@gmail.com>
2020-05-02 14:06:13 -0400

    Added Edit Contestant component

---
+++ src/app/modules/owner/components/edit-contestant/edit-contestant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-contestant',
+  templateUrl: './edit-contestant.component.html',
+  styleUrls: ['./edit-contestant.component.scss']
+})
+export class EditContestantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
