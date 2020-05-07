cb7562cb89fbfa67a869868fc64c2193dea3db59
AdrianSA <Adrian.angara@gmail.com>
2020-05-07 12:21:45 -0400

    Added View shows component on the owner module

---
+++ src/app/modules/owner/components/view-shows/view-shows.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-shows',
+  templateUrl: './view-shows.component.html',
+  styleUrls: ['./view-shows.component.scss']
+})
+export class ViewShowsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
