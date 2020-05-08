3674950e3ae46cebdb33bb5ba2c5086d47ea51cd
AdrianSA <Adrian.angara@gmail.com>
2020-05-08 14:51:50 -0400

    Added showcontestantfull interface

---
+++ src/app/modules/events/models/showcontestantfull.ts
@@ -0,0 +1,12 @@
+export interface ShowContestantFull {
+  contestantId: number;
+  showId: number;
+  firstname: string;
+  lastname: string;
+  name: string;
+  breed: string;
+  group: string;
+  isMale: boolean;
+  isSpecial: boolean;
+
+}
